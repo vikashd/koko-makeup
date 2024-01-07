0f90431c083199ef114379a6c667104ac88bbe75
Vikash Deepchand <vikash.deepchand@gmail.com>
2024-01-07 22:03:58 +0700

    Updated Footer

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -10,20 +10,20 @@ export function Footer() {
       <div className="container mx-auto">
         <div className="grid grid-cols-2 md:items-center md:grid-cols-3 gap-2 md:gap-4 pt-8 pb-12 border-t border-y-slate-50/75">
           <div>
-            <ul className="grid grid-cols-1 gap-2">
+            <ul className="inline-flex md:grid md:grid-cols-1 gap-2 md:gap-0">
               <li>
-                <Link href="/contact" className="inline-flex items-center">
-                  <Mail className="flex-shrink-0 mr-2" />{" "}
-                  <span className="text-sm">Contact</span>
+                <Link href="/contact" className="inline-flex items-center p-1">
+                  <Mail className="flex-shrink-0 md:mr-2" />{" "}
+                  <span className="text-sm hidden md:inline">Contact</span>
                 </Link>
               </li>
               <li>
                 <Link
                   href="tel:+447903444712"
-                  className="inline-flex items-center"
+                  className="inline-flex items-center p-1"
                 >
-                  <Phone className="flex-shrink-0 mr-2" />{" "}
-                  <span className="text-sm whitespace-nowrap">
+                  <Phone className="flex-shrink-0 md:mr-2" />{" "}
+                  <span className="text-sm whitespace-nowrap hidden md:inline">
                     +44 7903 444712
                   </span>
                 </Link>
