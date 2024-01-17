487ba9e207fde342bac8bcd0d919662f3477d23f
Vikash Deepchand <vikash.deepchand@gmail.com>
2024-01-17 21:51:46 +0700

    SSG gallery pages

--- src/app/(portfolio)/gallery/[id]/page.tsx
+++ src/app/(portfolio)/gallery/[id]/page.tsx
@@ -2,8 +2,9 @@ import { ArrowUpLeft } from "iconoir-react";
 import { Metadata, ResolvingMetadata } from "next";
 import Link from "next/link";
 import { cache } from "react";
-import { getGallery } from "@/app/_ctf/getGallery";
 import { Gallery as GalleryThumbs } from "@/app/_components/Gallery";
+import { getGallery } from "@/app/_ctf/getGallery";
+import { getGalleries } from "@/app/_ctf/getGalleries";
 
 interface PageProps {
   params: { id: string };
@@ -41,6 +42,16 @@ export async function generateMetadata(
       images: imageUrl ? `https:${imageUrl}?w=1600&h=900` : twitterImage,
     },
   };
+}
+
+export async function generateStaticParams() {
+  const data = await getGalleries({ include: 1, select: ["fields"] });
+
+  return (
+    data.items[0].fields.galleries?.reduce<{ id: string }[]>((acc, gallery) => {
+      return gallery ? acc.concat({ id: gallery.sys.id }) : acc;
+    }, []) || []
+  );
 }
 
 export default async function Gallery({ params: { id } }: PageProps) {
